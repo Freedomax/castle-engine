e8e23b4a5eebfa1da4d9fb87d7a230ffce901bef
Freedomax <576916445@qq.com>
2023-02-24 11:49:32 +0800

    Update buildTools.ps1

--- buildTools.ps1
+++ buildTools.ps1
@@ -5,9 +5,9 @@ Set-ExecutionPolicy Bypass -Scope Process
 
 $LAZBuildPath = "D:\soft\fpcupdeluxe\lazarus\lazbuild.exe"
 
-copy tools/build-tool/data/external_libraries/x86_64-win64/*.dll Bin/
-copy tools/build-tool/data/external_libraries/x86_64-win64/openssl/*.dll Bin/
-copy tools/build-tool/castle-engine.exe Bin/
+copy tools/build-tool/data/external_libraries/x86_64-win64/*.dll bin/
+copy tools/build-tool/data/external_libraries/x86_64-win64/openssl/*.dll bin/
+copy tools/build-tool/castle-engine.exe bin/
 
 & $LAZBuildPath -B "packages/castle_base.lpk"
 & $LAZBuildPath -B "packages/castle_components.lpk"
@@ -27,15 +27,15 @@ copy tools/build-tool/castle-engine.exe Bin/
 
 
 
-copy tools/castle-curves/castle-curves.exe Bin/
-copy tools/castle-editor/castle-editor.exe Bin/
-copy tools/image-to-pascal/image-to-pascal.exe Bin/
-copy tools/internal/x3d-nodes-to-pascal/x3d-nodes-to-pascal.exe Bin/
-copy tools/texture-font-to-pascal/texture-font-to-pascal.exe Bin/
-copy tools/to-data-uri/to-data-uri.exe Bin/
+copy tools/castle-curves/castle-curves.exe bin/
+copy tools/castle-editor/castle-editor.exe bin/
+copy tools/image-to-pascal/image-to-pascal.exe bin/
+copy tools/internal/x3d-nodes-to-pascal/x3d-nodes-to-pascal.exe bin/
+copy tools/texture-font-to-pascal/texture-font-to-pascal.exe bin/
+copy tools/to-data-uri/to-data-uri.exe bin/
 
-copy ../castle-view-image/castle-view-image.exe Bin/
-copy ../view3dscene/view3dscene.exe Bin/
-copy ../view3dscene/tovrmlx3d.exe Bin/
+copy ../castle-view-image/castle-view-image.exe bin/
+copy ../view3dscene/view3dscene.exe bin/
+copy ../view3dscene/tovrmlx3d.exe bin/
 
 pause
