12d6833b79cdfcb1b21a90b51669c7157a61b89b
Freedomax <fa_k@qq.com>
2023-04-11 17:46:21 +0800

    Optimize `TAnimationPlayer.MaxTime`.

--- src/base/castleanimationplayer.pas
+++ src/base/castleanimationplayer.pas
@@ -7,40 +7,58 @@ uses
   Generics.Collections, CastleTimeUtils, Rtti;
 
 type
-  TAnimationKeyframe = record
-    Time: TFloatTime;
-    Value: TValue;
-  end;
-
   TAnimationTrack = class
   private
+    FOnChange: TNotifyEvent;
+  type
+    TAnimationKeyframe = record
+      Time: TFloatTime;
+      Value: TValue;
+    end;
+
+    TAnimationKeyframeList = class(
+      {$IFDEF FPC}specialize{$ENDIF} TList<TAnimationKeyframe>)
+    end;
+
+  var
     FComponent: TPersistent;
     FProperty: string;
-    FKeyframes: array of TAnimationKeyframe;
+    FKeyframes: TAnimationKeyframeList;
+    procedure FKeyframesNotify(ASender: TObject; const AItem: TAnimationKeyframe;
+      AAction: TCollectionNotification);
     function Interpolate(const Keyframe1, Keyframe2: TAnimationKeyframe;
       const Time: TFloatTime): TValue;
+    procedure SetOnChange(const AValue: TNotifyEvent);
   public
     constructor Create(AComponent: TPersistent; const AProperty: string);
-    procedure AddKeyframe(Time: TFloatTime; const Value: TValue);
-    procedure Evaluate(Time: TFloatTime);
+    destructor Destroy; override;
+    procedure AddKeyframe(ATime: TFloatTime; const AValue: TValue);
+    procedure Evaluate(const ATime: TFloatTime);
     function Duration: TFloatTime;
+
+    property OnChange: TNotifyEvent read FOnChange write SetOnChange;
   end;
 
   TAnimationTrackList = class(
-{$IFDEF FPC}specialize{$ENDIF} TObjectList<TAnimationTrack>)
+    {$IFDEF FPC}specialize{$ENDIF} TObjectList<TAnimationTrack>)
   end;
 
   TAnimationPlayer = class(TCastleComponent)
   private
+    FMaxTime: TFloatTime;
     FTrackList: TAnimationTrackList;
     FCurrentTime: TFloatTime;
     FPlaying: boolean;
     FLoop: boolean;
     FSpeed: single;
+    procedure FTrackListNotify(ASender: TObject; const AItem: TAnimationTrack;
+      AAction: TCollectionNotification);
     procedure SetPlaying(const Value: boolean);
     procedure SetLoop(const Value: boolean);
     procedure SetSpeed(const Value: single);
     function GetMaxTime: TFloatTime;
+    procedure Changed;
+    procedure TrackChange(Sender: TObject);
   public
     constructor Create(AOwner: TComponent); override;
     destructor Destroy; override;
@@ -52,7 +70,7 @@ type
     procedure Stop;
     function PropertySections(const PropertyName: string): TPropertySections; override;
 
-    property MaxTime: TFloatTime read GetMaxTime;
+    property MaxTime: TFloatTime read FMaxTime;
   published
     property Playing: boolean read FPlaying write SetPlaying default False;
     property Loop: boolean read FLoop write SetLoop default False;
@@ -67,21 +85,34 @@ uses Math, TypInfo;
 constructor TAnimationTrack.Create(AComponent: TPersistent; const AProperty: string);
 begin
   inherited Create;
+  FKeyframes := TAnimationKeyframeList.Create;
+  FKeyframes.OnNotify :=
+    {$IFDEF FPC}
+   @
+     {$ENDIF}
+    FKeyframesNotify;
   FComponent := AComponent;
   FProperty := AProperty;
-  SetLength(FKeyframes, 0);
 end;
 
-procedure TAnimationTrack.AddKeyframe(Time: TFloatTime; const Value: TValue);
+destructor TAnimationTrack.Destroy;
 begin
-  SetLength(FKeyframes, Length(FKeyframes) + 1);
-  FKeyframes[High(FKeyframes)].Time := Time;
-  FKeyframes[High(FKeyframes)].Value := Value;
+  FKeyframes.Free;
+  inherited Destroy;
 end;
 
-procedure TAnimationTrack.Evaluate(Time: TFloatTime);
+procedure TAnimationTrack.AddKeyframe(ATime: TFloatTime; const AValue: TValue);
+var
+  Keyframe: TAnimationKeyframe;
+begin
+  Keyframe.Time := ATime;
+  Keyframe.Value := AValue;
+  FKeyframes.Add(Keyframe);
+end;
 
-  procedure SetProperty(AProperty: string; AValue: TValue);
+procedure TAnimationTrack.Evaluate(const ATime: TFloatTime);
+
+  procedure SetProperty(const AProperty: string; const AValue: TValue);
   var
     ctx: TRttiContext;
     t: TRttiType;
@@ -110,33 +141,33 @@ var
   I: integer;
   AValue: TValue;
 begin
-  if Length(FKeyframes) = 0 then
+  if FKeyframes.Count = 0 then
     Exit;
 
-  if Time < FKeyframes[0].Time then
+  if ATime < FKeyframes[0].Time then
   begin
     AValue := FKeyframes[0].Value;
     SetProperty(FProperty, AValue);
     Exit;
   end;
 
-  for I := Low(FKeyframes) to High(FKeyframes) - 1 do
-    if (Time >= FKeyframes[I].Time) and (Time < FKeyframes[I + 1].Time) then
+  for I := 0 to FKeyframes.Count - 2 do
+    if (ATime >= FKeyframes[I].Time) and (ATime < FKeyframes[I + 1].Time) then
     begin
-      AValue := Interpolate(FKeyframes[I], FKeyframes[I + 1], Time);
+      AValue := Interpolate(FKeyframes[I], FKeyframes[I + 1], ATime);
       SetProperty(FProperty, AValue);
       Exit;
     end;
 
-  AValue := FKeyframes[High(FKeyframes)].Value;
+  AValue := FKeyframes.Last.Value;
   SetProperty(FProperty, AValue);
 end;
 
 function TAnimationTrack.Duration: TFloatTime;
 begin
-  if Length(FKeyframes) < 2 then
+  if FKeyframes.Count < 2 then
     Exit(0);
-  Result := FKeyframes[High(FKeyframes)].Time - FKeyframes[Low(FKeyframes)].Time;
+  Result := FKeyframes.Last.Time - FKeyframes.First.Time;
 end;
 
 procedure TAnimationPlayer.SetPlaying(const Value: boolean);
@@ -146,6 +177,12 @@ begin
     FPlaying := Value;
     if FPlaying then FCurrentTime := 0;
   end;
+end;
+
+procedure TAnimationPlayer.FTrackListNotify(ASender: TObject;
+  const AItem: TAnimationTrack; AAction: TCollectionNotification);
+begin
+  Changed;
 end;
 
 procedure TAnimationPlayer.SetLoop(const Value: boolean);
@@ -168,7 +205,13 @@ constructor TAnimationPlayer.Create(AOwner: TComponent);
 begin
   inherited;
   FTrackList := TAnimationTrackList.Create(True);
+  FTrackList.OnNotify :=
+    {$IFDEF FPC}
+    @
+     {$ENDIF}
+    FTrackListNotify;
   FCurrentTime := 0;
+  FMaxTime := 0;
   FPlaying := False;
   FLoop := False;
   FSpeed := 1;
@@ -183,6 +226,11 @@ end;
 procedure TAnimationPlayer.AddTrack(const Track: TAnimationTrack);
 begin
   FTrackList.Add(Track);
+  Track.OnChange :=
+    {$IFDEF FPC}
+    @
+     {$ENDIF}
+    TrackChange;
 end;
 
 procedure TAnimationPlayer.RemoveTrack(const Track: TAnimationTrack);
@@ -229,10 +277,20 @@ begin
   Result := 0;
   for I := 0 to FTrackList.Count - 1 do
   begin
-    Track := TAnimationTrack(FTrackList[I]);
+    Track := FTrackList[I];
     if Track.Duration > Result then
       Result := Track.Duration;
   end;
+end;
+
+procedure TAnimationPlayer.Changed;
+begin
+  FMaxTime := GetMaxTime;
+end;
+
+procedure TAnimationPlayer.TrackChange(Sender: TObject);
+begin
+  Changed;
 end;
 
 function TAnimationTrack.Interpolate(const Keyframe1, Keyframe2: TAnimationKeyframe;
@@ -263,13 +321,26 @@ begin
     raise Exception.Create('Unsupported value type.');
 end;
 
+procedure TAnimationTrack.SetOnChange(const AValue: TNotifyEvent);
+begin
+  if FOnChange = AValue then Exit;
+  FOnChange := AValue;
+end;
+
+procedure TAnimationTrack.FKeyframesNotify(ASender: TObject;
+  const AItem: TAnimationKeyframe; AAction: TCollectionNotification);
+begin
+  if Assigned(FOnChange) then FOnChange(Self);
+end;
+
 procedure TAnimationPlayer.Stop;
 begin
   FCurrentTime := 0;
   FPlaying := False;
 end;
 
-function TAnimationPlayer.PropertySections(const PropertyName: string): TPropertySections;
+function TAnimationPlayer.PropertySections(
+  const PropertyName: string): TPropertySections;
 begin
   if ArrayContainsString(PropertyName, ['Playing', 'Loop', 'Speed']) then
     Result := [psBasic]
