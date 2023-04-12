49625b39d1b0288bcab1e9a7b87781321718c008
Freedomax <fa_k@qq.com>
2023-04-13 03:29:49 +0800

    Clean example.

--- examples/animations/test-animationplayer/code/gameviewmain.pas
+++ examples/animations/test-animationplayer/code/gameviewmain.pas
@@ -191,24 +191,6 @@ begin
   Result := inherited;
   if Result then Exit; // allow the ancestor to handle keys
 
-  { This virtual method is executed when user presses
-    a key, a mouse button, or touches a touch-screen.
-
-    Note that each UI control has also events like OnPress and OnClick.
-    These events can be used to handle the "press", if it should do something
-    specific when used in that UI control.
-    The TViewMain.Press method should be used to handle keys
-    not handled in children controls.
-  }
-
-  // Use this to handle keys:
-  {
-  if Event.IsKey(keyXxx) then
-  begin
-    // DoSomething;
-    Exit(true); // key was handled
-  end;
-  }
 end;
 
 procedure TLabelColorTrack.SetValue(const AValue: variant);
