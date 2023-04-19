32a54b3c1b17f13145a61a38086069603dfda931
Freedomax <fa_k@qq.com>
2023-04-20 04:51:11 +0800

    Improve discrete mode.

--- src/base/castleanimationplayer.pas
+++ src/base/castleanimationplayer.pas
@@ -75,7 +75,8 @@ type
   strict private
     FOnChange: TNotifyEvent;
     FKeyframeList: TAnimationKeyframeList;
-    FMode: TAnimationTrackMode;
+    { For mode @link(tmDiscrete), no need to execute every frame, so check the last executed frame. }
+    FLastExecutedKeyFrame: TAnimationKeyFrame;
     procedure KeyFramInTrackChange(ASender: TObject;
       const AChangeType: TKeyFrameChangeType);
     procedure KeyframesNotify(ASender: TObject;
@@ -88,6 +89,7 @@ type
     { This notification is used by @link(TAnimation), please do not use it. }
     property OnChange: TNotifyEvent read FOnChange write SetOnChange;
   strict protected
+    FMode: TAnimationTrackMode;
     FFriendlyObjectName: string;
     function GetFriendlyObjectName: string; virtual;
     procedure SetValue(const AValue: variant); virtual; abstract;
@@ -544,21 +546,39 @@ procedure TAnimationTrack.Evaluate(const ATime: TFloatTime);
 var
   AValue: variant;
   Index: SizeInt;
+  AKeyFrame: TAnimationKeyframe;
 begin
   if FKeyframeList.Count = 0 then
     Exit;
 
   if ATime < FKeyframeList.First.Time then
-    AValue := FKeyframeList.First.Value
+  begin
+    AKeyFrame := FKeyframeList.First;
+    AValue := FKeyframeList.First.Value;
+  end
   else
   begin
     Index := FKeyframeList.TimeToKeyFrame(ATime);
     if Between(Index, 0, FKeyframeList.Count - 2) then
-      AValue := Interpolate(FKeyframeList[Index], FKeyframeList[Index + 1], ATime)
+    begin
+      AKeyFrame := FKeyframeList[Index];
+      AValue := Interpolate(FKeyframeList[Index], FKeyframeList[Index + 1], ATime);
+    end
     else
+    begin
       { If the time is before the first frame or after the last frame, it's considered as the last static frame.}
+      AKeyFrame := FKeyframeList.Last;
       AValue := FKeyframeList.Last.Value;
+    end;
   end;
+
+  if (FMode = tmDiscrete) then
+  begin
+    if (FLastExecutedKeyFrame = AKeyFrame) then Exit
+    else
+      FLastExecutedKeyFrame := AKeyFrame;
+  end;
+
   SetValue(AValue);
 end;
 
@@ -866,10 +886,17 @@ end;
 procedure TAnimationTrack.KeyFramInTrackChange(ASender: TObject;
   const AChangeType: TKeyFrameChangeType);
 begin
-  if AChangeType = TKeyFrameChangeType.kfcTime then
-  begin
-    FKeyframeList.Sort;
-    if Assigned(FOnChange) then FOnChange(Self);
+  case AChangeType of
+    TKeyFrameChangeType.kfcTime:
+    begin
+      FKeyframeList.Sort;
+      FLastExecutedKeyFrame := nil;
+      if Assigned(FOnChange) then FOnChange(Self);
+    end;
+    TKeyFrameChangeType.kfcValue:
+      FLastExecutedKeyFrame := nil;
+    TKeyFrameChangeType.kfcLerpFunc: ;
+    else;
   end;
 end;
 
@@ -1261,7 +1288,8 @@ begin
   inherited Destroy;
 end;
 
-function TAnimationPlayer.PropertySections(const PropertyName: string): TPropertySections;
+function TAnimationPlayer.PropertySections(const PropertyName: string):
+TPropertySections;
 begin
   if ArrayContainsString(PropertyName, ['Playing', 'Animation']) then
     Result := [psBasic]
